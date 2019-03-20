0d828ecd43a8de751308bce58ead7ce5cd4a3be5
Olujimi Olugboyega <ololugbo@Olujimis-MBP.guest.corp.microsoft.com>
2019-03-20 15:31:53 -0700

    Fixed the errors concerning the logger script

--- DefineExtraLoggerProperties.sh
+++ DefineExtraLoggerProperties.sh
@@ -1,12 +1,9 @@
 #Load /etc/environment so we have $SPARK_HOME set
 source /etc/environment
 
-echo "About to make changes to the logger file"
-echo "The spark home about to be set is $SPARK_HOME"
-
 #overwrite the former log4j properties with the new one
 sudo sed -i "s/log4jspark.log.dir=.*/log4jspark.log.dir=\/var\/log\/sparkapp\/\${user.name}/g" $SPARK_HOME/conf/log4j.properties
-sudo sed -i "s/log4jspark.log.file=.*/log4jspark.log.file=\/var\/log\/sparkapp\/sanity_test/spark.log/g" $SPARK_HOME/conf/log4j.properties
+sudo sed -i "s/log4jspark.log.file=.*/log4jspark.log.file=spark.log/g" $SPARK_HOME/conf/log4j.properties
 sudo sed -i 's/log4jspark.root.logger=.*/log4jspark.root.logger=INFO,console,DRFA/g' $SPARK_HOME/conf/log4j.properties                                             
 sudo sed -i 's/log4j.appender.DRFA.DatePattern=.*/log4j.appender.DRFA.DatePattern=.yyyyMMdd/g' $SPARK_HOME/conf/log4j.properties 
 sudo sed -i 's/log4j.appender.DRFA.layout=.*/log4j.appender.DRFA.layout=org.apache.log4j.PatternLayout/g' $SPARK_HOME/conf/log4j.properties
