1a6bf387b8b8ef161d53fe27dd8d368fe24e8036
Olujimi Olugboyega <ololugbo@Olujimis-MacBook-Pro.local>
2019-04-19 15:51:07 -0700

    Fixed the entry script

--- EntryScripts/AirflowEdgeNodeSetup.sh
+++ EntryScripts/AirflowEdgeNodeSetup.sh
@@ -9,18 +9,18 @@ declare P_RABBITMQ_VHOST=$8
 declare P_RABBITMQ_USERNAME=$9
 declare P_RABBITMQ_PASSWORD=${10}
 declare P_RABBITMQ_HOSTNAME=${11}
-declare P_SA_USERNAME=${12}
+declare P_SA_EMAIL=${12}
 declare P_SA_PASSWORD=${13}
 
 # Setup the python environment
-source InstallAndConfigurePyEnv.sh $P_SSH_USERNAME $P_PYTHON_VERSION
+source InstallAndConfigureDevelopmentTools.sh $P_SSH_USERNAME $P_PYTHON_VERSION
 
 # Install airflow on the python environment
 source InstallAirflowWithDependencies.sh
 
 # Define the environment variables needed to connect with the other resources in our 
 # Celery executor architecture
-source DefineAirflowEnvironmentVariables.sh $P_SSH_USERNAME $P_AIRFLOW_DATABASE $P_MYSQL_SERVER_USERNAME $P_MYSQL_SERVER_PASSWORD $P_MYSQL_SERVER_HOSTNAME $P_RABBITMQ_QUEUENAME $P_RABBITMQ_VHOST $P_RABBITMQ_USERNAME $P_RABBITMQ_PASSWORD $P_RABBITMQ_HOSTNAME $P_SA_USERNAME $P_SA_PASSWORD
+source DefineAirflowEnvironmentVariables.sh $P_SSH_USERNAME $P_AIRFLOW_DATABASE $P_MYSQL_SERVER_USERNAME $P_MYSQL_SERVER_PASSWORD $P_MYSQL_SERVER_HOSTNAME $P_RABBITMQ_QUEUENAME $P_RABBITMQ_VHOST $P_RABBITMQ_USERNAME $P_RABBITMQ_PASSWORD $P_RABBITMQ_HOSTNAME $P_SA_EMAIL $P_SA_PASSWORD
 
 unset P_SSH_USERNAME
 unset P_PYTHON_VERSION
@@ -33,5 +33,5 @@ unset P_RABBITMQ_VHOST
 unset P_RABBITMQ_USERNAME
 unset P_RABBITMQ_PASSWORD
 unset P_RABBITMQ_HOSTNAME
-unset P_SA_USERNAME
+unset P_SA_EMAIL
 unset P_SA_PASSWORD
