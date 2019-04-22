81630e71830f3ce3df21cf73058fe2421d074f80
Olujimi Olugboyega <ololugbo@Olujimis-MacBook-Pro.local>
2019-04-22 10:56:50 -0700

    Added versions to the airflow libraries

--- Python/Airflow/InstallAirflowWithDependencies.sh
+++ Python/Airflow/InstallAirflowWithDependencies.sh
@@ -1,9 +1,16 @@
-SLUGIFY_USES_TEXT_UNIDECODE=yes pip install apache-airflow
-
-pip install azure
-
-pip install celery
-
+export SLUGIFY_USES_TEXT_UNIDECODE=yes
 sudo apt-get -y install libmysqlclient-dev
 
-pip install mysqlclient
+declare -a PIP_INSTALL=(
+apache-airflow==1.10.2
+azure==4.0.0
+celery==4.3.0
+mysqlclient==1.4.2
+)
+
+for package in ${PIP_INSTALL[@]}; do
+    echo "Installing $package ..."
+    sudo -E pip install $package
+done
+
+unset PIP_INSTALL
