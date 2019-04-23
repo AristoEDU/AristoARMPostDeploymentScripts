bcb555e05c9349c9f48a84debe508788c353b755
Olujimi Olugboyega <ololugbo@Olujimis-MacBook-Pro.local>
2019-04-23 12:23:19 -0700

    Added the hard coded versions back

--- Python/Airflow/InstallAirflowWithDependencies.sh
+++ Python/Airflow/InstallAirflowWithDependencies.sh
@@ -1,6 +1,6 @@
-SLUGIFY_USES_TEXT_UNIDECODE=yes pip install apache-airflow
+SLUGIFY_USES_TEXT_UNIDECODE=yes pip install apache-airflow==1.10.2
 
-pip install celery
+pip install celery==4.2.2
 
 sudo apt-get -y install libmysqlclient-dev
-pip install mysqlclient
+pip install mysqlclient==1.4.2
