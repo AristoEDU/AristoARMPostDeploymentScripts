06071e7aa9de1e91ca0062e71728b5ad3b13adbc
Ubuntu <sshuser@hn0-devana.fkn1sc1lvhpuvmwkkpxeh24r1c.gx.internal.cloudapp.net>
2019-06-06 17:46:28 +0000

    add colors for scala and check if home already exists for the user

--- hdinsight/add_user_account.sh
+++ hdinsight/add_user_account.sh
@@ -35,36 +35,49 @@ if [ ! -d ~/.vim ]; then
    mkdir -p ~/.vim/{ftdetect,indent,syntax} && for d in ftdetect indent syntax ; do curl -o ~/.vim/$d/scala.vim https://raw.githubusercontent.com/derekwyatt/vim-scala/master/$d/scala.vim; done
 fi
 
-if [ "${CURR_PATH}" != "${HOME}" ]; then
-   echo "WARNING: Replacing .bashrc and .profile"
-   if [ -f ".bashrc" ]; then
-     mv ${HOME}/.bashrc ${HOME}/.bashrc_${TIMESTAMP} 
-     cp .bashrc ${HOME}/.
+if [ -f ~/.profile ]; then
+   res=$(grep Dscala ~/.profile)
+   if [ "${res}" == "" ]; then
+       echo "" >> ~/.profile
+       echo "alias scala=\"scala -Dscala.color\"" >> ~/.profile
+       echo "alias spark-shell=\"spark-shell --conf spark.driver.extraJavaOptions='-Dscala.color'\"" >> ~/.profile
    fi
-
-   if [ -f ".profile" ]; then 
-     mv ${HOME}/.profile ${HOME}/.profile_${TIMESTAMP}
-     cp .profile ${HOME}/.
-   fi
-
-else
-   echo "at home path ..."
 fi
 
-sudo useradd  ${NEW_USER}
-echo -e "${DEFAULT_PASS}\n${DEFAULT_PASS}" | (sudo passwd ${NEW_USER})
-sudo passwd --maxdays 99999 --warndays 99900 ${NEW_USER}
-sudo usermod -a -G sshuser ${NEW_USER}
+if [ ! -d /home/${NEW_USER} ]; then
+  ### copy provided .bashrc and .profile
+  if [ "${CURR_PATH}" != "${HOME}" ]; then
+     echo "WARNING: Replacing .bashrc and .profile"
+     if [ -f ".bashrc" ]; then
+       mv ${HOME}/.bashrc ${HOME}/.bashrc_${TIMESTAMP} 
+       cp .bashrc ${HOME}/.
+     fi
 
-sudo mkdir /home/${NEW_USER}
-sudo cp ${HOME}/.bashrc /home/${NEW_USER}/.
-sudo cp ${HOME}/.profile /home/${NEW_USER}/.
-sudo cp -r ${HOME}/.vim  /home/${NEW_USER}/.
-sudo cp ${HOME}/.vimrc /home/${NEW_USER}/.
+     if [ -f ".profile" ]; then 
+       mv ${HOME}/.profile ${HOME}/.profile_${TIMESTAMP}
+       cp .profile ${HOME}/.
+     fi
 
-sudo chown -R ${NEW_USER}:sshuser /home/${NEW_USER}
+  else
+     echo "at home path ..."
+  fi
 
-sudo chmod -R 544 /home/${NEW_USER}/.bashrc
-sudo chmod -R 544 /home/${NEW_USER}/.profile
-sudo chmod 750 /home/${NEW_USER}
+  sudo useradd  ${NEW_USER}
+  echo -e "${DEFAULT_PASS}\n${DEFAULT_PASS}" | (sudo passwd ${NEW_USER})
+  sudo passwd --maxdays 99999 --warndays 99900 ${NEW_USER}
+  sudo usermod -a -G sshuser ${NEW_USER}
 
+  sudo mkdir /home/${NEW_USER}
+  sudo cp ${HOME}/.bashrc /home/${NEW_USER}/.
+  sudo cp ${HOME}/.profile /home/${NEW_USER}/.
+  sudo cp -r ${HOME}/.vim  /home/${NEW_USER}/.
+  sudo cp ${HOME}/.vimrc /home/${NEW_USER}/.
+
+  sudo chown -R ${NEW_USER}:sshuser /home/${NEW_USER}
+
+  sudo chmod -R 544 /home/${NEW_USER}/.bashrc
+  sudo chmod -R 544 /home/${NEW_USER}/.profile
+  sudo chmod 750 /home/${NEW_USER}
+else
+   echo "User home /home/${NEW_USER} already exist."
+fi
