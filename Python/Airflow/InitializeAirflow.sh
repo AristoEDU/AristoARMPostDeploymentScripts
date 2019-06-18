d954a4845ee8f8bbc4063d0654fad3719794befc
Colin Armstrong <colinarm@microsoft.com>
2019-06-18 14:27:17 -0700

    Fix typos and remove 'P_' prefix for some variables.

--- Python/Airflow/InitializeAirflow.sh
+++ Python/Airflow/InitializeAirflow.sh
@@ -1,13 +1,13 @@
-declare P_ENVIRONMENT=$1
-declare P_GIT_USER=$2
-declare P_GIT_PASSWORD=$3
+declare ENVIRONMENT=$1
+declare GIT_USER=$2
+declare GIT_PASSWORD=$3
 
 set -xe
-myemail="${P_GIT_USER}@microsoft.com"
+myemail="${GIT_USER}@microsoft.com"
 
 #We'll use the HTTPS to push a ssh key to git, SSH for pull/push configuration
 gitrepo_ssh="git@github.com:AristoEDU/AristoAirflow.git"
-gitrepo_https="https://github.com/${P_GIT_USER}/AristoEDU/AristoAirflow.git"
+gitrepo_https="https://github.com/${GIT_USER}/AristoEDU/AristoAirflow.git"
 
 #Generating SSH key:
 ssh-keygen -f "${HOME}/.ssh/id_rsa" -t rsa -b 4096 -C "${myemail}" -N ''
@@ -19,7 +19,7 @@ git_api_addkey="https://api.$(echo ${gitrepo_https} |cut -d'/' -f3)/user/keys"
 git_ssl_keyname="$(hostname)_$(date +%d-%m-%Y)"
 
 #Post this ssh key:
-curl -u "${P_GIT_USER}:${P_GIT_PASSWORD}" -X POST -d "{\"title\":\"${git_ssl_keyname}\",\"key\":\"${sslpub}\"}" ${git_api_addkey}
+curl -u "${GIT_USER}:${GIT_PASSWORD}" -X POST -d "{\"title\":\"${git_ssl_keyname}\",\"key\":\"${sslpub}\"}" ${git_api_addkey}
 
 # Write out Host key checking
 ssh-keyscan -H 192.30.253.113 >> ~/.ssh/known_hosts
@@ -30,12 +30,12 @@ git clone $gitrepo_ssh
 
 cd ~/AristoAirflow
 
-# We can't seem to import the variables unles the db is inited but
+# We can't seem to import the variables unless the db is initialized but
 # airflow imports the dags as part of the initdb process, causing
-# and error so we have to init, import and init again.
+# an error so we have to init, import and init again.
 airflow initdb
 
-if [ "$P_ENVIRONMENT" = "Prod" ]
+if [ "$ENVIRONMENT" = "Prod" ]
   airflow variables --i ./AristoVariables.var
 else
   airflow variables --i ./AristoVariables-Dev.var
@@ -43,6 +43,6 @@ fi
 
 airflow initdb
 
-unset P_ENVIRONMENT
-unset P_GIT_USER
-unset P_GIT_PASSWORD
+unset ENVIRONMENT
+unset GIT_USER
+unset GIT_PASSWORD
