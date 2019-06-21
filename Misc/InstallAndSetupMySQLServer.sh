54a0748480530ac21779970363b99331502a5c32
Colin Armstrong <colinarm@microsoft.com>
2019-06-21 12:16:48 -0700

    Adding appropriate quotes around MySQL command.

--- Misc/InstallAndSetupMySQLServer.sh
+++ Misc/InstallAndSetupMySQLServer.sh
@@ -3,6 +3,6 @@ set -xe
 sudo apt-get update
 sudo apt-get install -y mysql-server-5.7
 
-mysql -e CREATE DATABASE AristoAirflow;
-mysql -e CREATE DATABASE AristoAirflowBackfill;
-mysql -e CREATE DATABASE edu_analytics;
+mysql -e "CREATE DATABASE AristoAirflow;"
+mysql -e "CREATE DATABASE AristoAirflowBackfill;"
+mysql -e "CREATE DATABASE edu_analytics;"
