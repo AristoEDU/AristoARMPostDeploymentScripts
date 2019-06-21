c3d88ea293226b267cb8071f0185989249401764
Colin Armstrong <colinarm@microsoft.com>
2019-06-21 09:16:13 -0700

    Adding SQL calls to create our current three databases.

---
+++ Misc/InstallAndSetupMySQLServer.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install -y mysql-server-5.7
+
+mysql -e CREATE DATABASE AristoAirflow;
+mysql -e CREATE DATABASE AristoAirflowBackfill;
+mysql -e CREATE DATABASE edu_analytics;
